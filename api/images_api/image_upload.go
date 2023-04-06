09d9fc4aa4296be3a256fb59084b756f87679fde
zxc <15660589213@163.com>
2023-04-06 20:21:08 +0800

    api:图片上传-白名单设置

--- api/images_api/image_upload.go
+++ api/images_api/image_upload.go
@@ -12,12 +12,22 @@ import (
 	"go_server/models/res"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type ImageUploadResponse struct {
 	FilName   string
 	IsSuccess bool
 	Msg       string
+}
+
+var WhiteImageList = map[string]bool{
+	"jpg":  true,
+	"png":  true,
+	"jpeg": true,
+	"ico":  true,
+	"tiff": true,
+	"svg":  true,
 }
 
 func (ImageApi) ImageUploadView(c *gin.Context) {
@@ -38,7 +48,6 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 	imageList, ok := form.File["images"]
 	if !ok {
 		res.FailWithMsg("文件不存在", c)
-
 	}
 	//判断路径是否存在
 	fileInfo, err := os.Stat(filepath.Dir(global.Config.Local.Path))
@@ -53,11 +62,25 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 
 	var resList []ImageUploadResponse
 	for _, image := range imageList {
+
+		//白名单
+		fileName := image.Filename
+		nameList := strings.Split(fileName, ".")
+		//获取图片后缀
+		suffix := strings.ToLower(nameList[len(nameList)-1])
+		if !WhiteImageList[suffix] {
+			resList = append(resList, ImageUploadResponse{
+				fileName,
+				false,
+				"不支持上传此类型文件",
+			})
+			continue
+		}
 		//限制文件大小
 		fileSize := float64(image.Size) / float64(1024*1024)
 		if fileSize > float64(global.Config.Local.FileSize) {
 			resList = append(resList, ImageUploadResponse{
-				image.Filename,
+				fileName,
 				false,
 				fmt.Sprintf("图片大小为:%.2fMB,超过设定大小:%dMB", fileSize, global.Config.Local.FileSize),
 			})
@@ -68,7 +91,7 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 		err := c.SaveUploadedFile(image, filePath)
 		if err != nil {
 			resList = append(resList, ImageUploadResponse{
-				image.Filename,
+				fileName,
 				false,
 				err.Error(),
 			})
